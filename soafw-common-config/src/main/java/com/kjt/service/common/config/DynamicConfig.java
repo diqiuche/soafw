0504434d1382be2caca00a57603aaf55d48f251e
zhu.gongping <zhu.gongping@kjt.com>
2015-03-02 10:08:06 +0800

    新增动态配置支持profile机制

--- soafw-common-config/src/main/java/com/kjt/service/common/config/DynamicConfig.java
+++ soafw-common-config/src/main/java/com/kjt/service/common/config/DynamicConfig.java
@@ -24,288 +24,325 @@ import com.kjt.service.common.util.Constants;
 import com.kjt.service.common.util.StringUtils;
 
 /**
- * config.file.dir：配置文件路径
+ * 动态检测&加载变化内容<br>
+ * config.file.dir：配置文件路径<br>
+ * 默认为没有profile<br>
+ * 当没有设置profile时 <br>
+ * System.getProperty(CONFIG_DIR, CONFIG_DIR_DEF) + File.separator + _settingFileName +
+ * File.separator + "." + type; <br>
+ * 当设置了profile时 <br>
+ * System.getProperty(CONFIG_DIR, CONFIG_DIR_DEF) + File.separator + profiel+.+ _settingFileName +
+ * File.separator + "." + type;
  * 
  * @author alexzhu
  * 
  */
-public class DynamicConfig extends ConfigFileDict implements Constants, Configuration, IConfigListener {
-	/**
-	 * System.getProperty(CONFIG_DIR, CONFIG_DIR_DEF) + File.separator + _settingFileName + File.separator + "." + type;
-	 */
-	private String fileName = null;
-	private String _settingFileName = "common";
-	private String type = ConfigFileTypeDict.XML;
-	private Configuration delegate;
-	private String encoding = "utf-8";
-	private boolean delimiterParsingDisabled;
+public class DynamicConfig extends ConfigFileDict
+        implements
+            Constants,
+            Configuration,
+            IConfigListener {
 
-	@PostConstruct
-	public void init() {
-		this.fileName = System.getProperty(CONFIG_DIR, CONFIG_DIR_DEF) + File.separator + _settingFileName + "." + type;
-		delegate = this.build();
-		ConfigUtils.getConfigUtilsInstance().addListener(this);
-	}
+    /**
+     * 默认为没有profile<br>
+     * 当没有设置profile时 <br>
+     * System.getProperty(CONFIG_DIR, CONFIG_DIR_DEF) + File.separator + _settingFileName +
+     * File.separator + "." + type; <br>
+     * 当设置了profile时 <br>
+     * System.getProperty(CONFIG_DIR, CONFIG_DIR_DEF) + File.separator + profiel+.+ _settingFileName
+     * + File.separator + "." + type;
+     */
+    private String fileName = null;
+    private String _settingFileName = "common";
+    private String type = ConfigFileTypeDict.XML;
+    private Configuration delegate;
+    private String encoding = "utf-8";
+    private boolean delimiterParsingDisabled;
 
-	public Configuration build() {
+    @PostConstruct
+    public void init() {
 
-		PropertiesConfiguration config = new PropertiesConfiguration();
+        this.fileName =
+                System.getProperty(CONFIG_DIR, CONFIG_DIR_DEF) + File.separator + this.getProfile()
+                        + _settingFileName + "." + type;
+        delegate = this.build();
+        ConfigUtils.getConfigUtilsInstance().addListener(this);
+    }
 
-		if ("xml".equalsIgnoreCase(this.getType())) {
-			config = new XMLPropertiesConfiguration();
-		}
+    /**
+     * 支持profile机制
+     * 
+     * @return
+     */
+    private String getProfile() {
+        String profile = System.getProperty("profile");
+        if (profile != null) {
+            profile = profile + ".";
+        } else {
+            profile = "";
+        }
+        return profile;
+    }
 
-		config.setDelimiterParsingDisabled(isDelimiterParsingDisabled());
+    public Configuration build() {
 
-		FileInputStream fileInputStream = null;
+        PropertiesConfiguration config = new PropertiesConfiguration();
 
-		try {
-			fileInputStream = new FileInputStream(getFileName());
-			if (!StringUtils.isEmpty(this.getEncoding())) {
-				config.load(fileInputStream, this.getEncoding());
-			} else {
-				config.load(fileInputStream);
-			}
-			return config;
-		} catch (Exception e) {
-			InputStream is = this.getClass().getClassLoader().getResourceAsStream("META-INF/config/local/"+this._settingFileName+"."+this.getType());
-			if(is == null){
-				throw new RuntimeException(e);
-			}
-			try {
-				config.load(is);
-				return config;
-			} catch (ConfigurationException e1) {
-				throw new RuntimeException(e1);
-			}
-			finally{
-				try {
-					if (is != null)
-						is.close();
-				} catch (IOException ex) {
-				}
-			}
-			
-		} finally {
-			try {
-				if (fileInputStream != null)
-					fileInputStream.close();
-			} catch (IOException e) {
-			}
-		}
-	}
+        if ("xml".equalsIgnoreCase(this.getType())) {
+            config = new XMLPropertiesConfiguration();
+        }
 
-	/**
-	 * 配置文件sampleName
-	 * 
-	 * @param fileName
-	 */
-	public void setFileName(String fileName) {
-		this._settingFileName = fileName;
-	}
+        config.setDelimiterParsingDisabled(isDelimiterParsingDisabled());
 
-	public String getFileName() {
-		return fileName;
-	}
+        FileInputStream fileInputStream = null;
 
-	public String getType() {
-		return type;
-	}
+        try {
+            fileInputStream = new FileInputStream(getFileName());
+            if (!StringUtils.isEmpty(this.getEncoding())) {
+                config.load(fileInputStream, this.getEncoding());
+            } else {
+                config.load(fileInputStream);
+            }
+            return config;
+        } catch (Exception e) {
+            InputStream is =
+                    this.getClass()
+                            .getClassLoader()
+                            .getResourceAsStream(
+                                    "META-INF/config/local/" + this.getProfile()
+                                            + this._settingFileName + "." + this.getType());
+            if (is == null) {
+                throw new RuntimeException(e);
+            }
+            try {
+                config.load(is);
+                return config;
+            } catch (ConfigurationException e1) {
+                throw new RuntimeException(e1);
+            } finally {
+                try {
+                    if (is != null) is.close();
+                } catch (IOException ex) {}
+            }
 
-	/**
-	 * 配置文件类型［properties/xml］,默认为xml
-	 * 
-	 * @param type
-	 */
-	public void setType(String type) {
-		this.type = type;
-	}
+        } finally {
+            try {
+                if (fileInputStream != null) fileInputStream.close();
+            } catch (IOException e) {}
+        }
+    }
 
-	public String getEncoding() {
-		return encoding;
-	}
+    /**
+     * 配置文件sampleName
+     * 
+     * @param fileName
+     */
+    public void setFileName(String fileName) {
+        this._settingFileName = fileName;
+    }
 
-	public void setEncoding(String encoding) {
-		this.encoding = encoding;
-	}
+    public String getFileName() {
+        return fileName;
+    }
 
-	public boolean isDelimiterParsingDisabled() {
-		return delimiterParsingDisabled;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public void setDelimiterParsingDisabled(boolean delimiterParsingDisabled) {
-		this.delimiterParsingDisabled = delimiterParsingDisabled;
-	}
+    /**
+     * 配置文件类型［properties/xml］,默认为xml
+     * 
+     * @param type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	public synchronized void onUpdate(Configuration delegate) {
-		this.delegate = delegate;
-	}
+    public String getEncoding() {
+        return encoding;
+    }
 
-	public Configuration subset(String prefix) {
-		return delegate.subset(prefix);
-	}
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
 
-	public boolean isEmpty() {
-		return delegate.isEmpty();
-	}
+    public boolean isDelimiterParsingDisabled() {
+        return delimiterParsingDisabled;
+    }
 
-	public boolean containsKey(String key) {
-		return delegate.containsKey(key);
-	}
+    public void setDelimiterParsingDisabled(boolean delimiterParsingDisabled) {
+        this.delimiterParsingDisabled = delimiterParsingDisabled;
+    }
 
-	public void addProperty(String key, Object value) {
-		delegate.addProperty(key, value);
-	}
+    public synchronized void onUpdate(Configuration delegate) {
+        this.delegate = delegate;
+    }
 
-	public void setProperty(String key, Object value) {
-		delegate.setProperty(key, value);
-	}
+    public Configuration subset(String prefix) {
+        return delegate.subset(prefix);
+    }
 
-	public void clearProperty(String key) {
-		delegate.clearProperty(key);
-	}
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
 
-	public void clear() {
-		delegate.clear();
-	}
+    public boolean containsKey(String key) {
+        return delegate.containsKey(key);
+    }
 
-	public Object getProperty(String key) {
-		return delegate.getProperty(key);
-	}
+    public void addProperty(String key, Object value) {
+        delegate.addProperty(key, value);
+    }
 
-	public Iterator getKeys(String prefix) {
-		return delegate.getKeys(prefix);
-	}
+    public void setProperty(String key, Object value) {
+        delegate.setProperty(key, value);
+    }
 
-	public Iterator getKeys() {
-		return delegate.getKeys();
-	}
+    public void clearProperty(String key) {
+        delegate.clearProperty(key);
+    }
 
-	public Properties getProperties(String key) {
-		return delegate.getProperties(key);
-	}
+    public void clear() {
+        delegate.clear();
+    }
 
-	public boolean getBoolean(String key) {
-		return delegate.getBoolean(key);
-	}
+    public Object getProperty(String key) {
+        return delegate.getProperty(key);
+    }
 
-	public boolean getBoolean(String key, boolean defaultValue) {
-		return delegate.getBoolean(key, defaultValue);
-	}
+    public Iterator getKeys(String prefix) {
+        return delegate.getKeys(prefix);
+    }
 
-	public Boolean getBoolean(String key, Boolean defaultValue) {
-		return delegate.getBoolean(key, defaultValue);
-	}
+    public Iterator getKeys() {
+        return delegate.getKeys();
+    }
 
-	public byte getByte(String key) {
-		return delegate.getByte(key);
-	}
+    public Properties getProperties(String key) {
+        return delegate.getProperties(key);
+    }
 
-	public byte getByte(String key, byte defaultValue) {
-		return delegate.getByte(key, defaultValue);
-	}
+    public boolean getBoolean(String key) {
+        return delegate.getBoolean(key);
+    }
 
-	public Byte getByte(String key, Byte defaultValue) {
-		return delegate.getByte(key, defaultValue);
-	}
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return delegate.getBoolean(key, defaultValue);
+    }
 
-	public double getDouble(String key) {
-		return delegate.getDouble(key);
-	}
+    public Boolean getBoolean(String key, Boolean defaultValue) {
+        return delegate.getBoolean(key, defaultValue);
+    }
 
-	public double getDouble(String key, double defaultValue) {
-		return delegate.getDouble(key, defaultValue);
-	}
+    public byte getByte(String key) {
+        return delegate.getByte(key);
+    }
 
-	public Double getDouble(String key, Double defaultValue) {
-		return delegate.getDouble(key, defaultValue);
-	}
+    public byte getByte(String key, byte defaultValue) {
+        return delegate.getByte(key, defaultValue);
+    }
 
-	public float getFloat(String key) {
-		return delegate.getFloat(key);
-	}
+    public Byte getByte(String key, Byte defaultValue) {
+        return delegate.getByte(key, defaultValue);
+    }
 
-	public float getFloat(String key, float defaultValue) {
-		return delegate.getFloat(key, defaultValue);
-	}
+    public double getDouble(String key) {
+        return delegate.getDouble(key);
+    }
 
-	public Float getFloat(String key, Float defaultValue) {
-		return delegate.getFloat(key, defaultValue);
-	}
+    public double getDouble(String key, double defaultValue) {
+        return delegate.getDouble(key, defaultValue);
+    }
 
-	public int getInt(String key) {
-		return delegate.getInt(key);
-	}
+    public Double getDouble(String key, Double defaultValue) {
+        return delegate.getDouble(key, defaultValue);
+    }
 
-	public int getInt(String key, int defaultValue) {
-		return delegate.getInt(key, defaultValue);
-	}
+    public float getFloat(String key) {
+        return delegate.getFloat(key);
+    }
 
-	public Integer getInteger(String key, Integer defaultValue) {
-		return delegate.getInteger(key, defaultValue);
-	}
+    public float getFloat(String key, float defaultValue) {
+        return delegate.getFloat(key, defaultValue);
+    }
 
-	public long getLong(String key) {
-		return delegate.getLong(key);
-	}
+    public Float getFloat(String key, Float defaultValue) {
+        return delegate.getFloat(key, defaultValue);
+    }
 
-	public long getLong(String key, long defaultValue) {
-		return delegate.getLong(key, defaultValue);
-	}
+    public int getInt(String key) {
+        return delegate.getInt(key);
+    }
 
-	public Long getLong(String key, Long defaultValue) {
-		return delegate.getLong(key, defaultValue);
-	}
+    public int getInt(String key, int defaultValue) {
+        return delegate.getInt(key, defaultValue);
+    }
 
-	public short getShort(String key) {
-		return delegate.getShort(key);
-	}
+    public Integer getInteger(String key, Integer defaultValue) {
+        return delegate.getInteger(key, defaultValue);
+    }
 
-	public short getShort(String key, short defaultValue) {
-		return delegate.getShort(key, defaultValue);
-	}
+    public long getLong(String key) {
+        return delegate.getLong(key);
+    }
 
-	public Short getShort(String key, Short defaultValue) {
-		return delegate.getShort(key, defaultValue);
-	}
+    public long getLong(String key, long defaultValue) {
+        return delegate.getLong(key, defaultValue);
+    }
 
-	public BigDecimal getBigDecimal(String key) {
-		return delegate.getBigDecimal(key);
-	}
+    public Long getLong(String key, Long defaultValue) {
+        return delegate.getLong(key, defaultValue);
+    }
 
-	public BigDecimal getBigDecimal(String key, BigDecimal defaultValue) {
-		return delegate.getBigDecimal(key, defaultValue);
-	}
+    public short getShort(String key) {
+        return delegate.getShort(key);
+    }
 
-	public BigInteger getBigInteger(String key) {
-		return delegate.getBigInteger(key);
-	}
+    public short getShort(String key, short defaultValue) {
+        return delegate.getShort(key, defaultValue);
+    }
 
-	public BigInteger getBigInteger(String key, BigInteger defaultValue) {
-		return delegate.getBigInteger(key, defaultValue);
-	}
+    public Short getShort(String key, Short defaultValue) {
+        return delegate.getShort(key, defaultValue);
+    }
 
-	public String getString(String key) {
-		return delegate.getString(key);
-	}
+    public BigDecimal getBigDecimal(String key) {
+        return delegate.getBigDecimal(key);
+    }
 
-	public String getString(String key, String defaultValue) {
-		return delegate.getString(key, defaultValue);
-	}
+    public BigDecimal getBigDecimal(String key, BigDecimal defaultValue) {
+        return delegate.getBigDecimal(key, defaultValue);
+    }
 
-	public String[] getStringArray(String key) {
-		return delegate.getStringArray(key);
-	}
+    public BigInteger getBigInteger(String key) {
+        return delegate.getBigInteger(key);
+    }
 
-	public List getList(String key) {
-		return delegate.getList(key);
-	}
+    public BigInteger getBigInteger(String key, BigInteger defaultValue) {
+        return delegate.getBigInteger(key, defaultValue);
+    }
 
-	public List getList(String key, List defaultValue) {
-		return delegate.getList(key, defaultValue);
-	}
+    public String getString(String key) {
+        return delegate.getString(key);
+    }
 
-	public Configuration getConfig(){
-		return delegate;
-	}
+    public String getString(String key, String defaultValue) {
+        return delegate.getString(key, defaultValue);
+    }
+
+    public String[] getStringArray(String key) {
+        return delegate.getStringArray(key);
+    }
+
+    public List getList(String key) {
+        return delegate.getList(key);
+    }
+
+    public List getList(String key, List defaultValue) {
+        return delegate.getList(key, defaultValue);
+    }
+
+    public Configuration getConfig() {
+        return delegate;
+    }
 }
