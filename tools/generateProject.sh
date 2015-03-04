e007ca2a6490916b780132717c14622e999f7ad5
zhu.gongping <zhu.gongping@kjt.com>
2015-03-04 19:18:43 +0800

    add -DarchetypeCatalog=locale to mvn archetype:generate

--- tools/generateProject.sh
+++ tools/generateProject.sh
@@ -15,47 +15,47 @@ sed -i "" "s/<packaging>jar<\/packaging>/<packaging>pom<\/packaging>/g" pom.xml
 #sed -i  "s/<packaging>jar<\/packaging>/<packaging>pom<\/packaging>/g" pom.xml 
 
 projectcommon=$projectid-common
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectcommon 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectcommon 
 echo $projectcommon build success
 
 projectconfig=$projectid-config
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectconfig 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectconfig 
 echo $projectconfig build success
 
 projectcache=$projectid-cache
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectcache 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectcache 
 echo $projectcache build success
 
 projectdao=$projectid-dao
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectdao 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectdao 
 echo $projectdao build success
 
 projectrpc=$projectid-rpc
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectrpc 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectrpc 
 echo $projectrpc build success
 
 projectmq=$projectid-mq
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectmq 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectmq 
 echo $projectmq build success
 
 projectdomain=$projectid-domain
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectdomain 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectdomain 
 echo $projectdomain build success
 
 projectservice=$projectid-service
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectservice 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectservice 
 echo $projectservice build success
 
 projectjob=$projectid-job
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectjob 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectjob 
 echo $projectjob build success
 
 projectservice=$projectid-service-impl
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectservice 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectservice 
 echo $projectservice build success
 
 projectwebid=$projectid-web
-mvn archetype:generate -DgroupId=com.kjt.service.$projectid -DartifactId=$projectwebid -DarchetypeArtifactId=maven-archetype-webapp 
+mvn archetype:generate -DarchetypeCatalog=locale -DgroupId=com.kjt.service.$projectid -DartifactId=$projectwebid -DarchetypeArtifactId=maven-archetype-webapp 
 echo $projectwebid build success
 
 ##配置文件生成
