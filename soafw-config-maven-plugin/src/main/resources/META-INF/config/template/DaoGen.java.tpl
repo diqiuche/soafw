133494e5d4bdb3797f3312b303742ba6b3bc6c13
zhu.gongping <zhu.gongping@kjt.com>
2015-03-05 12:46:22 +0800

    新增数据访问层代码生成器模版

--- soafw-config-maven-plugin/src/main/resources/META-INF/config/template/DaoGen.java.tpl
+++ soafw-config-maven-plugin/src/main/resources/META-INF/config/template/DaoGen.java.tpl
@@ -4,16 +4,14 @@ import com.kjt.service.common.dao.generate.tool.DaoGenFromDB;
 
 public class DaoGen {
 
-    public class GenImplTest {
 
-        public static void main(String[] args) {
-            try {
-                DaoGenFromDB.generateDAO("数据库名", "表名", "classpath:spring-dao.gen.xml",
-                        "com.kjt.common.tsl", "src/main/java/", "src/main/resources/");
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+    public static void main(String[] args) {
+        try {
+            DaoGenFromDB.generateDAO("数据库名", "表名", "classpath:spring-dao.gen.xml",
+                    "com.kjt.common.tsl", "src/main/java/", "src/main/resources/");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
--- tools/generateProject.sh
+++ tools/generateProject.sh
@@ -73,13 +73,20 @@ mvn soafw-config:config -Dtemplate=pom.xml.config -DartifactId=$projectid -Ddest
 
 ##cache
 mvn soafw-config:config -Dtemplate=pom.xml.cache -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-cache
+mvn soafw-config:config -Dsufix=md -Dtemplate=README.md.cache -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-cache
 mvn soafw-config:config -Dtemplate=cache.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-cache/src/main/resource/META-INF/config/local
+mvn soafw-config:config -Dsufix=properties -Dtemplate=cache-mem.properties -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-cache/src/main/resource/META-INF/config/local
+mvn soafw-config:config -Dsufix=properties -Dtemplate=cache-redis.properties -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-cache/src/main/resource/META-INF/config/local
 mvn soafw-config:config -Dtemplate=spring-cache.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-cache/src/main/resource/META-INF/config/spring
 
 ##dao
 mvn soafw-config:config -Dtemplate=pom.xml.dao -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-dao
+mvn soafw-config:config -Dsufix=md -Dtemplate=README.md.dao -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-dao
 mvn soafw-config:config -Dtemplate=acc.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-dao/src/main/resource/META-INF/config/local
+mvn soafw-config:config -Dsufix=properties -Dtemplate=database.properties -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-dao/src/main/resource/META-INF/config/local
 mvn soafw-config:config -Dtemplate=spring-db.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-dao/src/main/resource/META-INF/config/spring
+mvn soafw-config:config -Dtemplate=spring-dao.gen.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-dao/src/main/resource
+mvn soafw-config:config -Dsufix=java -Dtemplate=DaoGen.java -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-dao/src/test/java/com/kjt/service/tsl/dao
 
 
 ##rpc
@@ -106,8 +113,8 @@ mvn soafw-config:config -Dtemplate=spring-job.xml -DartifactId=$projectid -Ddest
 ##web
 mvn soafw-config:config -Dtemplate=pom.xml.web -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-web
 mvn soafw-config:config -Dtemplate=web.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-web/src/main/resource/META-INF/config/spring
-mvn soafw-config:config -Dtemplate=application-config.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-web/src/main/resource/META-INF/config/local
-mvn soafw-config:config -Dtemplate=mvc-config.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-web/src/main/webapp/WEB-INF
+mvn soafw-config:config -Dtemplate=spring-web.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-web/src/main/resource/META-INF/config/local
+mvn soafw-config:config -Dtemplate=spring-mvc.xml -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-web/src/main/webapp/WEB-INF
 
 ##service impl
 mvn soafw-config:config -Dtemplate=pom.xml.svc.impl -DartifactId=$projectid -DdestDir=../../projects/$projectid/$projectid-service-impl
