fd73d6e2617eace659dd4aed80cdc4d826547eb2
zhu.gongping <zhu.gongping@kjt.com>
2015-04-17 17:30:21 +0800

    AbsServiceImpl 为非动态加载服务

--- soafw-common-service-impl/src/main/java/com/kjt/service/common/impl/AbsServiceImpl.java
+++ soafw-common-service-impl/src/main/java/com/kjt/service/common/impl/AbsServiceImpl.java
@@ -1,13 +1,15 @@
 package com.kjt.service.common.impl;
 
 import com.kjt.service.common.IService;
-import com.kjt.service.common.config.DynamicConfig;
+import com.kjt.service.common.log.Logger;
+import com.kjt.service.common.log.LoggerFactory;
 import com.kjt.service.common.result.IResult;
 
-public abstract class AbsServiceImpl<T extends IResult> extends DynamicConfig implements IService<T> {
+public abstract class AbsServiceImpl<T extends IResult> implements IService<T> {
 	/**
 	 * Logger for this class
 	 */
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public AbsServiceImpl(){
 	    
