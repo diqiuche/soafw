e3da4dec8a8dd0a2968a1a1ef10d177e7b4d3afa
zhu.gongping <zhu.gongping@kjt.com>
2015-06-03 14:58:47 +0800

    fix startService_org.sh 模版 bug（cd ..;~）

--- tools/publish/startService_org.sh
+++ tools/publish/startService_org.sh
@@ -12,6 +12,5 @@ echo "prefix_service_pid:"$$prefix_service_pid
  [ -n "$"$prefix"_service_pid" ] && kill -9 $$prefix_service_pid
 
 tar -zxvf ../$prefix-service-impl/$prefix-service-impl-1.0-SNAPSHOT-bin.tar.gz
-cd ../prefix-service-impl/lib
 
 nohup java -Dapp.home.dir=$app_home_dir -jar $prefix-service-impl-1.0-SNAPSHOT/lib/$prefix-service-impl-1.0-SNAPSHOT.jar >/dev/null  &
