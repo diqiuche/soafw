d4c14787587926abedff05af4a890eb293e1aee4
zhu.gongping <zhu.gongping@kjt.com>
2015-06-08 14:22:57 +0800

    attach client req to service

--- soafw-common-service/src/main/java/com/kjt/service/common/filter/ServiceFilter.java
+++ soafw-common-service/src/main/java/com/kjt/service/common/filter/ServiceFilter.java
@@ -1,5 +1,7 @@
 package com.kjt.service.common.filter;
 
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.Filter;
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
@@ -8,20 +10,30 @@ import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.RpcException;
 import com.kjt.service.common.log.Logger;
 import com.kjt.service.common.log.LoggerFactory;
+import com.kjt.service.common.util.RequestID;
 
+@Activate(group = {Constants.PROVIDER,Constants.CONSUMER})
 public class ServiceFilter implements Filter {
     private Logger logger = LoggerFactory.getLogger(getClass());
-    private String reqidKey = "SvcReqId";
+    private String reqidKey = "ReqId";
     
     public ServiceFilter(){
-        System.out.println("ServiceFilter created");
+        logger.info("ServiceFilter created");
     }
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        boolean provider = RpcContext.getContext().isProviderSide();
+        RpcContext context = RpcContext.getContext();
+        String remoteIp = context.getRemoteHost();
+        boolean provider = context.isProviderSide();
         if(provider){
-            String ReqId = invoker.getUrl().getParameter("ReqId");
-            System.out.println(ReqId);
+            String reqId = context.getAttachment(reqidKey);
+            RequestID.set(reqId);
+            logger.info(reqidKey+" from client@"+remoteIp);
+        }
+        else{
+            String reqId = RequestID.get();
+            context.setAttachment(reqidKey, reqId);
+            logger.info(reqidKey+" to service@"+remoteIp);
         }
         Result result = invoker.invoke(invocation);
         return result;
